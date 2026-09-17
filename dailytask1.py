2dc784b821b7430a322342ec848ebbb572efd3b1
ADARSH M <adarshmohanann@gmail.com>
2026-09-17 16:40:46 +0530

    Add files via upload

---
+++ dailytask1.py
@@ -0,0 +1,50 @@
+# 1) check if a number is a mutiple of 5
+
+# num = int(input("Enter a number : "))
+# if num % 5 == 0:
+#     print("Multiple of 5")
+# else:
+#     print("Not a multiple of 5")
+
+
+# 2) area of a triangle
+
+
+# num1 = int(input("Enter length:"))
+# num2 = int(input("Enter Breadth:"))
+# print("area of triangle is", 1/2*num1*num2)
+
+
+# 3)  perimeter of circle
+
+
+# num = int(input("Enter radius:"))
+# print("perimeter of the circle is",2*num,"pi")
+
+
+# 4) square of a number
+
+# num=int(input("Enter a number:"))
+# print("Square of the numberis ",num**2)
+
+
+# 5) convert dollor to Inr
+
+
+# num=int(input("Enter amount:"))
+# print("Inr value is ",num*96)
+
+
+# 6) miles to km
+
+# num = int(input("Enter miles:"))
+# print("Total KM is", num*1.6)
+
+
+# 7) Check if a number is positive or negative
+
+# num = int(input("Enter a number:"))
+# if num < 0:
+#     print(num, "is Negative")
+# else:
+#     print(num, "is positive")
